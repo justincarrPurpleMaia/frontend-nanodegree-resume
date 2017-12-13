d14b97da5b9a24c00ce8084ff8ba871fdac5cccd
justincarrPurpleMaia <kamaile@kamakaus-MacBook-Air.local>
2017-12-13 12:58:56 -1000

    added name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ /*$("#header").prepend("Justin Carr");
+var awesomethoughts =
+    "Hello I am Awesome!";
+
+var funthoughts =
+    awesomethoughts.replace("Awesome", "fun");
+
+$("#main").append(funthoughts); */
+            
+    /*var awesomethoughts = "I am justin and I am awesome!";
+
+    console.log(awesomethoughts); */
+var name = "Justin";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = "web devoloper";
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
